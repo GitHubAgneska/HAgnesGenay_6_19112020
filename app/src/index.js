e4618bcedb5351c742c2224fa78ad157f3fa8675
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2021-03-04 09:55:22 +0100

    JS - creating photgrapher factory

---
+++ app/src/index.js
@@ -0,0 +1,13 @@
+
+
+
+// Retrieve data from json
+//------------------------------
+
+// HOMEPAGE : photographers list
+//------------------------------
+
+
+// For each photographer of list, generate new photographer component
+// using class Photographer
+//------------------------------
